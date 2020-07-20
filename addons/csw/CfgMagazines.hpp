1cbeb83c0478761f642db57feffd4402d0e8db4a
Samuel Glew <samuel.glew@gmail.com>
2020-07-20 20:44:22 +0930

    adding medical .dll back, adding 40rnd GMG ammo belt

--- addons/csw/CfgMagazines.hpp
+++ addons/csw/CfgMagazines.hpp
@@ -62,6 +62,16 @@ class CfgMagazines {
         picture = QPATHTOF(UI\ammoBox_50bmg_ca.paa);
         type = 256;
         count = 20;
+        mass = 50;
+        ACE_isBelt = 1;
+    };
+    class GVAR(40Rnd_20mm_G_belt): 40Rnd_20mm_G_belt {
+        author = ECSTRING(common,ACETeam);
+        displayName = CSTRING(GMGBelt_displayName);
+        model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
+        picture = QPATHTOF(UI\ammoBox_50bmg_ca.paa);
+        type = 256;
+        count = 40;
         mass = 96;
         ACE_isBelt = 1;
     };
